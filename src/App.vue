57e163959118fbd8de050a61c5eabb19956db26f
oopchoi <oopchoi@gmail.com>
2019-03-02 19:35:31 +0900

    init commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style src="./style.scss" lang="scss"></style>
 
+<script>
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  metaInfo: {
+    title: 'Index',
+    titleTemplate: '%s - Appwork'
+  },
+  updated () {
+    // Remove loading state
+    setTimeout(() => document.body.classList.remove('app-loading'), 1)
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
